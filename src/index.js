48bb78479d811271429f35e25248f48ff7343216
pratyush-78 <pratyushanand78@gmail.com>
2022-01-17 01:16:12 +0530

    added index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 import React from "react";
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 import App from './App'
+import store from './App/Store'
 import 'antd/dist/antd.css' // make our app look so much better
 
 ReactDOM.render(
     <Router>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </Router>
     , document.getElementById('root')
 )
