798d8382264a4ee5724369b015f28002ef5d3718
pratyush-78 <pratyushanand78@gmail.com>
2022-01-16 18:32:35 +0530

    updated home & currency page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { Card, Row, Col, Input } from 'antd'
+
+import { useGetcryptosQuery } from '../services/CryptoApi'          // it contains all endpoints for our api call
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Cryptocurrencies = () => {
+
+
+    const {data:cryptosList, isFetching} = useGetcryptosQuery();           // de- structure, RENAME data as cryptosList
+
+    const [ cryptos, setCryptos ] = useState(cryptosList?.data?.coins);     // 'cryptos' will become array of coins (& its details)
+
+
+    if(isFetching)  return 'Loading...'
+    // console.log(cryptos)
     return (
         <div>
-            
+            <Row gutter={[30,30]} className='crypto-card-container' >
+                {cryptos?.map((currency) => (
+                    <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+                        <Link to="/">
+                        {/* <p>{currency.name}</p> */}
+                        <Card 
+                            title={`${currency.rank}. ${currency.name}`}
+                            extra={<img src={currency.iconUrl} className='crypto-image' alt=""/>}
+                            hoverable
+                            >
+                            <p>Price: {millify(currency.price)}</p>
+                            <p>Market Cap: {millify(currency.marketCap)}</p>
+                            <p>Daily Change: <span style={{color : `${currency.change<0 ? 'red':'#26a537'}`}}>{currency.change}</span>%</p>
+                            {/* <p>{currency.name}</p> */}
+                        </Card>
+                        
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
         </div>
     )
 }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic} from 'antd'
+import { Cryptocurrencies, News } from '../components';
+
+import { useGetcryptosQuery } from '../services/CryptoApi';
+
+
+const { Title } = Typography;   // de-structuring Typography.Title -> Title, so that we dont have to type alot 
 
 const Homepage = () => {
+
+    const { data, isFetching } = useGetcryptosQuery(50);              // call this as hook, then we immediately get the data
+    const globalStats = data?.data?.stats;                         // data obj > data > stats or data.data.stats
+
+    console.log(data);                                              // at start the data was undefined, so redux toolkit gives us
+                                                                    // the isFetching  state/var, 
+    if(isFetching)  return "Loading...";
+
     return (
         <div>
-            
+            <Title level={2} className='heading'>Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/> </Col>           {/*  in antd, there are total 24 spaces, so this (each col) takes half the width */}
+                {/* <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges}/> </Col>            */}
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/> </Col>           
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/> </Col>           
+                <Col span={12}><Statistic title="Total 24-hr Volume" value={millify(globalStats.total24hVolume)}/> </Col>           
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/> </Col>           
+            </Row>
+            <div className='home-heading container'>
+                <Title span={12} level={2} className='home-title'>Top 10 Cryptocurrencies</Title>
+                <Title span={12} level={3} className='show-more'><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            {/* simplified is used to render only 10 elements */}
+            <div className='home-heading container'>
+                <Title span={12} level={2} className='home-title'>Latest Crypto News</Title>
+                <Title span={12} level={3} className='show-more'><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified/>
         </div>
     )
 }
